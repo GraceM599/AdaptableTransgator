186f5a70db9aa599fc70271e3ecedb5feef2d33c
Grace Mihiral <mihiralgrace@gmail.com>
2026-03-11 18:33:31 -0400

    Added to HashMap

--- src/HashMap.h
+++ src/HashMap.h
@@ -11,21 +11,33 @@
 struct Slot {
     std::string en;
     std::string translation;
-    Slot(std::string e, std::string t) {
+    unsigned long long freq;
+    Slot(std::string e, std::string t, unsigned long long f) {
         this->en = std::move(e);
         this->translation = std::move(t);
+        this->freq = f;
     }
     Slot() {
         en = "";
         translation = "";
+        freq = 0;
     }
 };
 
-
+//Used hash function djb2 from http://www.cse.yorku.ca/%7Eoz/hash.html
+//Known to be good for strings
 class HashMap : dictionary{
     std::array<Slot, 500000> table;
-    size_t hash(const std::string& en) {
-        return 0;
+
+    unsigned long long hash(const std::string& en)
+    {
+        unsigned long hash = 5381;
+
+        for (const char c : en) {
+            hash = ((hash << 5) + hash) + c;
+        }
+
+        return hash;
     }
 public:
     HashMap() {
@@ -35,6 +47,7 @@ public:
     std::vector<std::string> getValues(std::string line) {
         std::vector<std::string> r = {};
         std::string curr = "";
+        line += ",";
         for (int i = 0; i<line.size(); ++i) {
             if (line[i] == ',') {
                 r.push_back(curr);
@@ -52,7 +65,7 @@ public:
         std::string line = "";
         while (std::getline(in, line)) {
             std::vector<std::string> values = getValues(line);
-            success = insert(values[0], values[1]);
+            success = insert(values[0], values[1], std::stoull(values[2]));
             if (!success) {
                 std::cout << "Error with line: " << line << std::endl;
             }
@@ -61,7 +74,7 @@ public:
 
     }
 
-    bool insert(std::string en, std::string trans) override {
+    bool insert(std::string en, std::string trans, unsigned long long freq) override {
         //modulus by M **
         int loc = hash(en) % table.size(); // ensure within bounds
 
@@ -72,6 +85,7 @@ public:
                 //Empty slot
                 table[index].en = en;
                 table[index].translation = trans;
+                table[index].freq = freq;
                 return true;
             }
 
@@ -84,7 +98,21 @@ public:
 
         }
     }
-    std::string search(std::string en) override;
+    std::string search(std::string en) override {
+        int loc = hash(en) % table.size(); // ensure within bounds
+
+        for (int i = 0; i < table.size(); ++i) {
+            int index = (loc + i) % table.size();
+
+            if (table[index].en == en) {
+                //key matches
+                return table[index].translation;
+            }
+
+        }
+        return "Not found in table";
+    }
+
 };
 
 
