e52f5c7298e28bf0ac821734b466210717c004d8
Grace Mihiral <mihiralgrace@gmail.com>
2026-03-13 10:53:16 -0400

    changed main vers

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,19 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <optional>
 
 int main()
 {
-	std::cout << "Launching TRANSGATOR SFML window...\n";
+	sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
 
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
-
-    while (window.isOpen())
-    {
-        while (auto event = window.pollEvent())
+	while (window.isOpen())
+	{
+		while (auto event = window.pollEvent())
 		{
 			if (event->is<sf::Event::Closed>())
 				window.close();
 		}
 
-        window.clear(sf::Color::Black);
-        window.display();
-    }
+		window.clear(sf::Color::Black);
+		window.display();
+	}
 }
