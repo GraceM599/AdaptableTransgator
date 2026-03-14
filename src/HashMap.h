82ed739ab27e673cab59bc5fe32d588caafc7c1c
Grace Mihiral <mihiralgrace@gmail.com>
2026-03-14 13:12:26 -0400

    Added comments on use of functions in HashMap.h

--- src/HashMap.h
+++ src/HashMap.h
@@ -31,21 +31,33 @@ struct Slot {
 //Known to be good for strings
 class HashMap : dictionary{
     std::array<Slot, 500000> table;
-
+    //takes string and returns the hash as a large number
     unsigned long long hash(const std::string& en);
 
 public:
+    //creates object and loads data into the map from dictionary.csv
     HashMap();
     ~HashMap();
+    //splits a csv line into english word, spanish word, and frequency then returns in a vector
     std::vector<std::string> getValues(std::string line);
+    //loads each line of csv into the object by inserting the word, translation, and freq
     bool loadData();
 
+    //inserts english, translation, and freq as an entry in the hash table
     bool insert(std::string en, std::string trans, unsigned long long freq) override;
+    //returns translation of english word through hash table lookup
+    //returns "Not found!" if english word is not in dictionary
     std::string search(std::string en) override;
+    //returns the Slot that the key en is stored at.
     Slot search_Slot(std::string en);
 
+    //returns a vector of <en, translation> pairs with a maximum size of 10 sorted by the frequency the english word is used
     std::vector<std::tuple<std::string, std::string>> prefixSearch(std::string en);
 
+    //returns the elapsed time of a specified function
+    // "constructor" -> hash map creation time
+    // "word search" -> single word translation time
+    // "prefix search" -> prefix search translation time
     double getFunctionTime(std::string func, std::string word = "");
 
 };
